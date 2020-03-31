1737321a5d1f187a48ce6105ebc16cf6d541707b
smallconan <zcb1693749255@outlook.com>
2020-03-31 13:31:11 +0800

    1.3

---
+++ js/blog.js
@@ -0,0 +1,14 @@
+$(".head-container").append(
+   "<div class=\"row\">\n" +
+    "        <div class=\"header-right\">\n" +
+    "                    <ul>\n" +
+    "                            <li><a  href=\"\\blog\">博客</a></li>\n" +
+    "                            <li><a href=\"dailyarticle.html\">每日一文</a></li>\n" +
+    "                            <li><a href=\"\\blog\\share\">分享</a></li>\n" +
+    "                       </ul>\n" +
+    "                </div>\n" +
+    "           <div class=\"header-left\">\n" +
+    "                   <a href=\"index.html\"><img src=\"imgs/special_photo_bg.png\" height=\"40\" width=\"40\" style=\"padding-top: 5px\"></a>\n" +
+    "               </div>\n" +
+    "     </div>"
+);
